e82c7208033c6a17da9eafd631169e38608971a3
Sean Grant <seangrant118@gmail.com>
2020-02-08 15:40:05 -0500

    flashcard deck comp

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,6 +1,7 @@
 import React from "react";
 import THE from "../grammer/DefiniteArticle";
-import IAm from '../grammer/IAm'
+import IAm from '../grammer/IAm';
+import FlashcardsDeck from './FlashcardsDeck';
 
 class Flashcards extends React.Component {
   state = {
@@ -23,30 +24,11 @@ class Flashcards extends React.Component {
   render() {
     return (
       <div>
-        <select value={this.state.topic} onChange={this.onTopicChange}>
-          <option value="Definite Article">The Definite Article</option>
-          <option value="I Am">I Am</option>
-        </select>
+        <FlashcardsDeck topic={this.state.topic} onTopicChange={this.onTopicChange}/>
         <button onClick={this.getFlashcards}>Get Flashcards</button>
       </div>
     );
   }
 }
-
-// const Flashcards = () => (
-//   <div>
-//     {THE.map(the => {
-//       return (
-//         <div>
-//           <p>{the.word}</p>
-//           <p>{the.case}</p>
-//           <p>{the.gender}</p>
-//           <p>{the.person}</p>
-//           <br></br>
-//         </div>
-//       );
-//     })}
-//   </div>
-// );
 
 export default Flashcards;
