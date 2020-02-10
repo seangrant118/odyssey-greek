a12006877f49680e068e6595c197b4f19f12009a
Sean Grant <seangrant118@gmail.com>
2020-02-10 17:40:23 -0500

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './styles/Header.css'
 
 const Header = () => (
-  <header>
-    <h1>Odyssey Greek</h1>
-    <NavLink to="/" activeClassName='is-active' exact={true}>Home </NavLink>
-    <NavLink to="/alphabet" activeClassName='is-active'>Ancient Greek Alphabet</NavLink>
-    <NavLink to="/flashcards" activeClassName='is-active'>flashcards</NavLink>
+  <header className='header'>
+    <h1 className='header-title'>Odyssey Greek</h1>
+    <NavLink 
+      className='header-links' 
+      to="/" 
+      exact={true}>Home 
+    </NavLink>
+    <NavLink 
+      className='header-links' 
+      to="/alphabet">
+      Ancient Greek Alphabet
+    </NavLink>
+    <NavLink 
+      className='header-links' 
+      to="/flashcards">
+        flashcards
+    </NavLink>
   </header>
 )
 
