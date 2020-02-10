558a0168c80a63102a609efe0a62386a38a2bcfd
Sean Grant <seangrant118@gmail.com>
2020-02-10 17:49:14 -0500

    styled select in flashcards

--- src/components/FlashcardsDeck.js
+++ src/components/FlashcardsDeck.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './styles/FlashcardsDeck.css'
 
 const FlashcardsDeck = (props) => (
   <div>
     <select
       value={props.topic}
       onChange={props.onTopicChange}
+      className='deck-select'
     >
       <option value='Definite Article'>The Definite Article</option>
       <option value='I Am'>I Am</option>
