3987e3e7e0ab58d2c1041bd5da528e494e516c01
Sean Grant <seangrant118@gmail.com>
2020-02-22 13:05:10 -0500

    definite article reformat

--- src/grammer/DefiniteArticle.js
+++ src/grammer/DefiniteArticle.js
@@ -2,87 +2,87 @@ const THE = [
   {
     id: 1,
     front: "ὁ",
-    back: ["Case: Nominative", "Gender: M", "Person: (s.)"]
+    back: ["Nominative", "M", "(s.)"]
   },
   {
     id: 2,
     front: "τόν",
-    back: ["Case: Accusative", "Gender: M", "Person: (s.)"]
+    back: ["Accusative", "M", "(s.)"]
   },
   {
     id: 3,
     front: "τοῠ",
-    back: ["Case: Genitive", "Gender: M or N", "Person: (s.)"]
+    back: ["Genitive", "M or N", "(s.)"]
   },
   {
     id: 4,
     front: "τῷ",
-    back: ["Case: Dative", "Gender: M or N", "Person: (s.)"]
+    back: ["Dative", "M or N", "(s.)"]
   },
   {
     id: 5,
     front: "ή",
-    back: ["Case: Nominative", "Gender: F", "Person: (s.)"]
+    back: ["Nominative", "F", "(s.)"]
   },
   {
     id: 6,
     front: "τήν",
-    back: ["Case: Accusative", "Gender: F", "Person: (s.)"]
+    back: ["Accusative", "F", "(s.)"]
   },
   {
     id: 7,
     front: "τῆς",
-    back: ["Case: Genitive", "Gender: F", "Person: (s.)"]
+    back: ["Genitive", "F", "(s.)"]
   },
   {
     id: 8,
     front: "τῇ",
-    back: ["Case: Dative", "Gender: F", "Person: (s.)"]
+    back: ["Dative", "F", "(s.)"]
   },
   {
     id: 9,
     front: "τό",
-    back: ["Case: Nominative or Accusative", "Gender: N", "Person: (s.)"]
+    back: ["Nominative or Accusative", "N", "(s.)"]
   },
   {
     id: 10,
     front: "οί",
-    back: ["Case: Nominative", "Gender: M", "Person: (pl.)"]
+    back: ["Nominative", "M", "(pl.)"]
   },
   {
     id: 11,
     front: "τούς",
-    back: ["Case: Accusative", "Gender: M", "Person: (pl.)"]
+    back: ["Accusative", "M", "(pl.)"]
   },
   {
     id: 12,
     front: "τῶν",
-    back: ["Case: Genative", "Gender: M or F or N", "Person: (pl.)"]
+    back: ["Genative", "M or F or N", "(pl.)"]
   },
   {
     id: 13,
     front: "τοῖς",
-    back: ["Case: Dative", "Gender: M or N", "Person: (pl.)"]
+    back: ["Dative", "M or N", "(pl.)"]
   },
   {
     id: 14,
     front: "αί",
-    back: ["Case: Nominatve", "Gender: F", "Person: (pl.)"]
+    back: ["Nominatve", "F", "(pl.)"]
   },
   {
     id: 15,
     front: "τᾱς",
-    back: ["Case: Accusative", "Gender: F", "Person: (pl.)"]
+    back: ["Accusative", "F", "(pl.)"]
   },
   {
     id: 16,
     front: "ταῖς",
-    back: ["Case: Dative", "Gender: F", "Person: (pl.)"]
+    back: ["Dative", "F", "(pl.)"]
   },
   {
     id: 17,
     front: "τά",
-    back: ["Case: Nominative or Accusative", "Gender: N", "Person: (pl.)"]
+    back: ["Nominative or Accusative", "N", "(pl.)"]
   }
 ];
 
