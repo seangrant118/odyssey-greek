561381e6128b2520a41a793d8fdc963869ca6fad
Sean Grant <seangrant118@gmail.com>
2020-02-22 13:04:10 -0500

    i am reformat

--- src/grammer/IAm.js
+++ src/grammer/IAm.js
@@ -2,32 +2,32 @@ const IAm = [
   {
     id: 1,
     front: "εἰμί",
-    back: ["Definiton: I Am", "Person: 1st", "Number: (s.)"]
+    back: ["I Am", "First Person", "(s)"]
   },
   {
     id: 2,
     front: "εἶ",
-    back: ["Definition: You are", "Person: 2nd", "Number: (s.)"]
+    back: ["You are", "Second Person", "(s)"]
   },
   {
     id: 3,
     front: "ἐστί(ν)",
-    back: ["Definition: He/She/It is", "Person: 3rd", "Number: (s.)"]
+    back: ["He/She/It is", "Third Person", "(s)"]
   },
   {
     id: 4,
     front: "ἐσμέν",
-    back: ["Definition: We are", "Person: 1st", "Number: (pl.)"]
+    back: ["We are", "First Person", "(pl)"]
   },
   {
     id: 5,
     front: "ἐστέ",
-    back: ["Definition: You are", "Person: 2nd", "Number: (pl.)"]
+    back: ["You are", "Second Person", "(pl)"]
   },
   {
     id: 6,
     front: "εἰσί(ν)",
-    back: ["Definition: They are", "Person: 3rd", "Number: (pl.)"]
+    back: ["They are", "Third Person", "(pl)"]
   }
 ];
 
