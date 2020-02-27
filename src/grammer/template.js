db7aa359b0a5b71390d6b60cdd1fc321b08ba4bd
Sean Grant <seangrant118@gmail.com>
2020-02-27 13:44:54 -0500

    text vocab 2a-d

---
+++ src/grammer/template.js
@@ -0,0 +1,287 @@
+const newDeck = {
+  id: 22,
+  name: "2A-D",
+  category: {
+    id: 4,
+    name: "Text Vocab"
+  },
+  cards: [
+    {
+      id: 1,
+      front: "ἀγαθός ή όν",
+      back: ["good, noble, courageous"]
+    },
+    {
+      id: 2,
+      front: "Ἀθηναῖος, ὁ",
+      back: ["Athenian (2a)"]
+    },
+    {
+      id: 3,
+      front: "ἄμα",
+      back: ["at the same time"]
+    },
+    {
+      id: 4,
+      front: "ἄναχωρέω",
+      back: ["retreat"]
+    },
+    {
+      id: 5,
+      front: "ἀπορέ-ω",
+      back: ["be at a loss; have no resources"]
+    },
+    {
+      id: 6,
+      front: "ἀπορί-ᾱ, ἠ",
+      back: ["perplexity; lack of provisions"]
+    },
+    {
+      id: 7,
+      front: "αὖθις",
+      back: ["again"]
+    },
+    {
+      id: 8,
+      front: "βάρβαρος, ὁ",
+      back: ["barbarian, foreigner (2a)"]
+    },
+    {
+      id: 9,
+      front: "βέβαιος (ᾱ) ον",
+      back: ["secure"]
+    },
+    {
+      id: 10,
+      front: "βραδέως",
+      back: ["slowly"]
+    },
+    {
+      id: 11,
+      front: "διά",
+      back: ["(+acc) because of"]
+    },
+    {
+      id: 12,
+      front: "διέρχομαι",
+      back: ["go through, relate"]
+    },
+    {
+      id: 13,
+      front: "δουλό-ομαι",
+      back: ["enslave"]
+    },
+    {
+      id: 14,
+      front: "ἐλευθερίᾱ, ἡ",
+      back: ["freedom"]
+    },
+    {
+      id: 15,
+      front: "ἐλεύθερος α ον",
+      back: ["free"]
+    },
+    {
+      id: 16,
+      front: "ἐλευθερόω",
+      back: ["free, set free"]
+    },
+    {
+      id: 17,
+      front: "ἐμός ή όν",
+      back: ["my; mine"]
+    },
+    {
+      id: 18,
+      front: "ἐπειδή",
+      back: ["when"]
+    },
+    {
+      id: 19,
+      front: "ἐπέρχομαι",
+      back: ["go against, attack"]
+    },
+    {
+      id: 20,
+      front: "ἐπί (+acc)",
+      back: ["at, against, to attack"]
+    },
+    {
+      id: 21,
+      front: "ἔρχομαι",
+      back: ["go, come"]
+    },
+    {
+      id: 22,
+      front: "ἡδέως",
+      back: ["with pleasure, happily"]
+    },
+    {
+      id: 23,
+      front: "ἥδη",
+      back: ["by now, now, already"]
+    },
+    {
+      id: 24,
+      front: "ἡσυχάζω",
+      back: ["be quiet, keep quiet"]
+    },
+    {
+      id: 25,
+      front: "ἡσυχίᾱ, ἡ",
+      back: ["quiet, peace (1b)"]
+    },
+    {
+      id: 26,
+      front: "θεά, ἡ",
+      back: ["goddess (1b)"]
+    },
+    {
+      id: 27,
+      front: "θεά-ομαι",
+      back: ["observe, watch"]
+    },
+    {
+      id: 28,
+      front: "κάλλιστος η ον",
+      back: ["superlative, beautiful, good, fine"]
+    },
+    {
+      id: 29,
+      front: "λόγος, ὁ",
+      back: ["story, tale"]
+    },
+    {
+      id: 30,
+      front: "μάχομαι",
+      back: ["fight"]
+    },
+    {
+      id: 31,
+      front: "ναυμαχίᾱ, ἡ",
+      back: ["naval battle"]
+    },
+    {
+      id: 32,
+      front: "νῑκάω",
+      back: ["win, defeat"]
+    },
+    {
+      id: 33,
+      front: "νῑκη, ἡ",
+      back: ["victory, conquest (1a)"]
+    },
+    {
+      id: 34,
+      front: "ὁμόνοια, ἡ",
+      back: ["agreement, harmony (1b)"]
+    },
+    {
+      id: 35,
+      front: "ὅσος η ον",
+      back: ["how great!"]
+    },
+    {
+      id: 36,
+      front: "οὐκέτι",
+      back: ["no longer"]
+    },
+    {
+      id: 37,
+      front: "οὕτω(ς)",
+      back: ["thus, so, in this way"]
+    },
+    {
+      id: 38,
+      front: "παρά",
+      back: ["(+ acc) along, alongside"]
+    },
+    {
+      id: 39,
+      front: "πίπτω",
+      back: ["fall, die"]
+    },
+    {
+      id: 40,
+      front: "πολέμιοι, οἱ",
+      back: ["the enemy (2a)"]
+    },
+    {
+      id: 41,
+      front: "πολέμιος ᾱ ον",
+      back: ["hosile, enemy"]
+    },
+    {
+      id: 42,
+      front: "πόλεμος, ὁ",
+      back: ["war"]
+    },
+    {
+      id: 43,
+      front: "πότερον... ἥ",
+      back: ["whether... or"]
+    },
+    {
+      id: 44,
+      front: "προσέρχομαι",
+      back: ["advance, go/come towards"]
+    },
+    {
+      id: 45,
+      front: "σιωπά-ω",
+      back: ["be silent"]
+    },
+    {
+      id: 46,
+      front: "σκοπέ-ω",
+      back: ["look (at), consider"]
+    },
+    {
+      id: 47,
+      front: "στρατιά, ἡ",
+      back: ["army (1b)"]
+    },
+    {
+      id: 48,
+      front: "ταχέως",
+      back: ["quickly"]
+    },
+    {
+      id: 49,
+      front: "τέλος",
+      back: ["in the end, finally"]
+    },
+    {
+      id: 50,
+      front: "τι",
+      back: ["a, something"]
+    },
+    {
+      id: 51,
+      front: "τόλμα, ἡ",
+      back: ["daring (1c)"]
+    },
+    {
+      id: 52,
+      front: "τολμάω",
+      back: ["dare, be daring, undertake"]
+    },
+    {
+      id: 53,
+      front: "φοβέομαι",
+      back: ["fear, be afraid (of)"]
+    },
+    {
+      id: 54,
+      front: "ψευδῶς",
+      back: ["falsely"]
+    },
+    {
+      id: 55,
+      front: "ὥσπερ",
+      back: ["like, as"]
+    }
+  ]
+};
+
+export default newDeck;
