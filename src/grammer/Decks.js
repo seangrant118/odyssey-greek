6da6eb0b43e6ca9741d11749303f3eb780b3d36f
Sean Grant <seangrant118@gmail.com>
2020-04-15 11:56:50 -0400

    unit 3

--- src/grammer/Decks.js
+++ src/grammer/Decks.js
@@ -2014,6 +2014,116 @@ const decks = [
       },
     ],
   },
+  {
+    id: 29,
+    name: "Unit #3",
+    category: {
+      id: 5,
+      name: "Key Words",
+    },
+    cards: [
+      {
+        id: 1,
+        front: "ὅδε, ἤδε, τόδε",
+        back: ["pron", "this"],
+      },
+      {
+        id: 2,
+        front: "τῆ",
+        back: ["dat fem of ὁ", "here, there"],
+      },
+      {
+        id: 3,
+        front: "ἀνήρ",
+        back: ["masc noun", "man"],
+      },
+      {
+        id: 4,
+        front: "λόγος",
+        back: ["masc noun", "word, reason, story"],
+      },
+      {
+        id: 5,
+        front: "ἐπεί",
+        back: ["conj", "after, since, when"],
+      },
+      {
+        id: 6,
+        front: "οὐδείς",
+        back: ["adj", "no one, none, nobody"],
+      },
+      {
+        id: 7,
+        front: "ὡς",
+        back: ["adv", "as, how"],
+      },
+      {
+        id: 8,
+        front: "γε",
+        back: ["part", "at least, at any rate"],
+      },
+      {
+        id: 9,
+        front: "φημί",
+        back: ["to declare, say"],
+      },
+      {
+        id: 10,
+        front: "ἐάν",
+        back: ["conj", "if"],
+      },
+      {
+        id: 11,
+        front: "ὅστις, ἥτις, ὅ τι",
+        back: ["pron", "anyone who, anything which"],
+      },
+      {
+        id: 12,
+        front: "νῦν",
+        back: ["adv", "now"],
+      },
+      {
+        id: 13,
+        front: "ἑαυτοῦ",
+        back: ["pron", "of himself, absolutely"],
+      },
+      {
+        id: 14,
+        front: "μέγας",
+        back: ["adj", "big, great, large"],
+      },
+      {
+        id: 15,
+        front: "τῷ",
+        back: ["dat. neut of τό", "therefore, thereupon"],
+      },
+      {
+        id: 16,
+        front: "ἐκεῖνος, ἐκεῖνη, ἐκεῖνον",
+        back: ["pron", "that person, that thing"],
+      },
+      {
+        id: 17,
+        front: "ἴσος",
+        back: ["adj", "equal, like"],
+      },
+      {
+        id: 18,
+        front: "οὖν",
+        back: ["adj", "therefore"],
+      },
+      {
+        id: 19,
+        front: "μετά",
+        back: ["prep + dat/gen/acc", "among, after, with"],
+      },
+      {
+        id: 20,
+        front: "ἄν",
+        back: ["part", "would"],
+      },
+    ],
+  },
 ];
 
 export default decks;
