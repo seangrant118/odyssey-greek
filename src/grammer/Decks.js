a340b04c573444653d354b6b6878dd3092825f25
Sean Grant <seangrant118@gmail.com>
2020-04-15 11:19:16 -0400

    unit 2

--- src/grammer/Decks.js
+++ src/grammer/Decks.js
@@ -1804,6 +1804,116 @@ const decks = [
       },
     ],
   },
+  {
+    id: 26,
+    name: "Unit #2",
+    category: {
+      id: 5,
+      name: "Key Words",
+    },
+    cards: [
+      {
+        id: 1,
+        front: "ἀλλά",
+        back: ["conj", "otherwise, but"],
+      },
+      {
+        id: 2,
+        front: "σύ",
+        back: ["pron", "you"],
+      },
+      {
+        id: 3,
+        front: "γίγνομαι",
+        back: ["to come into being, happen"],
+      },
+      {
+        id: 4,
+        front: "κατά",
+        back: ["prep + acc/gen", "down, downwards"],
+      },
+      {
+        id: 5,
+        front: "ἔχω",
+        back: ["to have, hold"],
+      },
+      {
+        id: 6,
+        front: "ἤ",
+        back: ["conj", "or, than"],
+      },
+      {
+        id: 7,
+        front: "πᾶς",
+        back: ["adj", "all"],
+      },
+      {
+        id: 8,
+        front: "μή",
+        back: ["not"],
+      },
+      {
+        id: 9,
+        front: "ἄλλος",
+        back: ["adj", "other, another"],
+      },
+      {
+        id: 10,
+        front: "περί",
+        back: ["prep + gen/dat/acc", "around"],
+      },
+      {
+        id: 11,
+        front: "πολύς",
+        back: ["adj", "many, much"],
+      },
+      {
+        id: 12,
+        front: "ὑπό",
+        back: ["prep + gen/dat/acc", "under, by, towards"],
+      },
+      {
+        id: 13,
+        front: "ποιέω",
+        back: ["to make, do"],
+      },
+      {
+        id: 14,
+        front: "λέγω",
+        back: ["to lay, choose, say"],
+      },
+      {
+        id: 15,
+        front: "ἕ",
+        back: ["pron", "himself/herself"],
+      },
+      {
+        id: 16,
+        front: "δή",
+        back: ["part", "indeed, in truth"],
+      },
+      {
+        id: 17,
+        front: "άπό",
+        back: ["prep + gen", "from, away from"],
+      },
+      {
+        id: 18,
+        front: "εἰ",
+        back: ["conj", "if, whether"],
+      },
+      {
+        id: 19,
+        front: "διά",
+        back: ["prep + gen/acc", "through, by means of"],
+      },
+      {
+        id: 20,
+        front: "πόλις",
+        back: ["f noun", "city"],
+      },
+    ],
+  },
 ];
 
 export default decks;
