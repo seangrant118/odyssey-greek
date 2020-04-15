e748a5f83c89768d0a5d221ffd029b3fb7f54fe8
Sean Grant <seangrant118@gmail.com>
2020-04-15 14:55:42 -0400

    keywords 4

--- src/grammer/Decks.js
+++ src/grammer/Decks.js
@@ -2124,6 +2124,116 @@ const decks = [
       },
     ],
   },
+  {
+    id: 30,
+    name: "Unit #4",
+    category: {
+      id: 5,
+      name: "Key Words",
+    },
+    cards: [
+      {
+        id: 1,
+        front: "παῖς",
+        back: ["m & f", "child"],
+      },
+      {
+        id: 2,
+        front: "ἀνά",
+        back: ["prep + gen/dat/acc", "up, upon"],
+      },
+      {
+        id: 3,
+        front: "τοιοῦτος",
+        back: ["adj", "such as this, so"],
+      },
+      {
+        id: 4,
+        front: "ὅσος",
+        back: ["adj", "as great as, how much"],
+      },
+      {
+        id: 5,
+        front: "θεός",
+        back: ["m", "God"],
+      },
+      {
+        id: 6,
+        front: "πρότερος",
+        back: ["adj", "before"],
+      },
+      {
+        id: 7,
+        front: "βασιλεύς",
+        back: ["m", "king"],
+      },
+      {
+        id: 8,
+        front: "λαμβάνω",
+        back: ["to take, recieve"],
+      },
+      {
+        id: 9,
+        front: "ἔτι",
+        back: ["adv", "yet, still"],
+      },
+      {
+        id: 10,
+        front: "καλέω",
+        back: ["to call, summon"],
+      },
+      {
+        id: 11,
+        front: "εἶς, μία, ἕν",
+        back: ["adj", "one"],
+      },
+      {
+        id: 12,
+        front: "παρά",
+        back: ["prep + gen/dat/acc", "beside"],
+      },
+      {
+        id: 13,
+        front: "δίδωμι",
+        back: ["to give"],
+      },
+      {
+        id: 14,
+        front: "γυνή",
+        back: ["f", "woman"],
+      },
+      {
+        id: 15,
+        front: "ὅτι",
+        back: ["conj", "that"],
+      },
+      {
+        id: 16,
+        front: "ἠμί",
+        back: ["to say"],
+      },
+      {
+        id: 17,
+        front: "δοκέω",
+        back: ["to think, suppose"],
+      },
+      {
+        id: 18,
+        front: "οὔτε",
+        back: ["adv", "and not, neither... nor"],
+      },
+      {
+        id: 19,
+        front: "οἷος",
+        back: ["pron", "such as, what kind"],
+      },
+      {
+        id: 20,
+        front: "εἶπον",
+        back: ["aor. of ἔπω", "to speak, say"],
+      },
+    ],
+  },
 ];
 
 export default decks;
