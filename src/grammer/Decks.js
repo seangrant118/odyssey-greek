515ddf16a0a713f5d1ccb947e4792d27bc58e5ba
Sean Grant <seangrant118@gmail.com>
2020-04-15 10:58:14 -0400

    unit one

--- src/grammer/Decks.js
+++ src/grammer/Decks.js
@@ -1694,6 +1694,116 @@ const decks = [
       },
     ],
   },
+  {
+    id: 25,
+    name: "Unit #1",
+    category: {
+      id: 5,
+      name: "Key Words",
+    },
+    cards: [
+      {
+        id: 1,
+        front: "ὁ, ἡ, τό",
+        back: ["the, that, he/she, it"],
+      },
+      {
+        id: 2,
+        front: "καί",
+        back: ["conj", "and, even"],
+      },
+      {
+        id: 3,
+        front: "δέ",
+        back: ["part", "but, on the other hand, then"],
+      },
+      {
+        id: 4,
+        front: "εἰμί",
+        back: ["to be"],
+      },
+      {
+        id: 5,
+        front: "εἰς",
+        back: ["prep + acc.", "into, to"],
+      },
+      {
+        id: 6,
+        front: "αὐτός, αὐτή, αὐτό",
+        back: ["pron", "self"],
+      },
+      {
+        id: 7,
+        front: "οὗτος, αὕτη, τοῦτο",
+        back: ["pron", "this"],
+      },
+      {
+        id: 8,
+        front: "μέν",
+        back: ["part", "but, on the one hand, then"],
+      },
+      {
+        id: 9,
+        front: "τις, τι",
+        back: ["part", "anyone, anything"],
+      },
+      {
+        id: 10,
+        front: "τε",
+        back: ["part", "and"],
+      },
+      {
+        id: 11,
+        front: "τίς, τί",
+        back: ["pron", "who? which?"],
+      },
+      {
+        id: 12,
+        front: "γάρ",
+        back: ["part", "for/because"],
+      },
+      {
+        id: 13,
+        front: "οὐ",
+        back: ["adv", "not"],
+      },
+      {
+        id: 14,
+        front: "ὅς, ἥ, ὅ",
+        back: ["pron", "this, that, who, which"],
+      },
+      {
+        id: 15,
+        front: "πρός",
+        back: ["prep + acc/gen/dat", "from, to, near"],
+      },
+      {
+        id: 16,
+        front: "ἐπί",
+        back: ["prep + acc/gen/dat", "on, upon"],
+      },
+      {
+        id: 17,
+        front: "ἐγώ",
+        back: ["pron", "I"],
+      },
+      {
+        id: 18,
+        front: "ὥς",
+        back: ["adv", "so, thus"],
+      },
+      {
+        id: 19,
+        front: "ἐν",
+        back: ["prep + dat", "in"],
+      },
+      {
+        id: 20,
+        front: "ἐκ",
+        back: ["prep + gen", "from, out of"],
+      },
+    ],
+  },
 ];
 
 export default decks;
