ca48d0e36ac25fd9fffbd21623972b0126389bf0
Sean Grant <seangrant118@gmail.com>
2020-04-30 17:35:45 -0400

    unit 5

--- src/grammer/template.js
+++ src/grammer/template.js
@@ -166,6 +166,117 @@ const keyWordTemplate = {
   ],
 };
 
+const keyWordTemplate2 = {
+  id: 33,
+  name: "Unit #5",
+  category: {
+    id: 5,
+    name: "Key Words",
+  },
+  cards: [
+    {
+      id: 1,
+      front: "ἔρχομαι",
+      back: ["to come, go"],
+    },
+    {
+      id: 2,
+      front: "δέω",
+      back: ["to bind, tie, lack, want"],
+    },
+    {
+      id: 3,
+      front: "δύο",
+      back: ["two"],
+    },
+    {
+      id: 4,
+      front: "σός",
+      back: ["adj", "thy, thine, your, yours"],
+    },
+    {
+      id: 5,
+      front: "καλός",
+      back: ["adj", "beautiful"],
+    },
+    {
+      id: 6,
+      front: "εἶμι",
+      back: ["to be"],
+    },
+    {
+      id: 7,
+      front: "ὥστε",
+      back: ["adv., conj", "as, as being, so as to"],
+    },
+    {
+      id: 8,
+      front: "ναῦς",
+      back: ["f.", "ship"],
+    },
+    {
+      id: 9,
+      front: "σφεῖς",
+      back: ["pron", "they"],
+    },
+    {
+      id: 10,
+      front: "ἕκαστος",
+      back: ["adj", "every, each"],
+    },
+    {
+      id: 11,
+      front: "ἑμός",
+      back: ["pron", "mine"],
+    },
+    {
+      id: 12,
+      front: "ἄνθροπος",
+      back: ["m", "man"],
+    },
+    {
+      id: 13,
+      front: "μόνος",
+      back: ["adj", "alone"],
+    },
+    {
+      id: 14,
+      front: "δίναμις",
+      back: ["f", "power, strength"],
+    },
+    {
+      id: 15,
+      front: "βούλομαι",
+      back: ["to wish, be wiling"],
+    },
+    {
+      id: 16,
+      front: "φιλος",
+      back: ["adj", "loved, dear, friend"],
+    },
+    {
+      id: 17,
+      front: "μάλιστα",
+      back: ["adv", "most, most of all"],
+    },
+    {
+      id: 18,
+      front: "ἔτερος",
+      back: ["adj", "the other, one of two"],
+    },
+    {
+      id: 19,
+      front: "οὐδέ",
+      back: ["part., adv", "but not, not even"],
+    },
+    {
+      id: 20,
+      front: "ἄγω",
+      back: ["to lead, carry, bring"],
+    },
+  ],
+};
+
 const nounTableTemplate = {
   id: 28,
   name: "3rd Declension A(2)",
