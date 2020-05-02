6d2f3f123c6b6345c79e1906ae558fb5699237f3
Sean Grant <seangrant118@gmail.com>
2020-05-01 20:44:51 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,6 @@ import "./styles/Header.css";
 const Header = () => (
   <header className="header">
     <h1 className="header-title">Odyssey Greek</h1>
-    <NavLink className="header-links" to="/" exact={true}>
-      Home
-    </NavLink>
-    <NavLink className="header-links" to="/alphabet">
-      Ancient Greek Alphabet
-    </NavLink>
-    <NavLink className="header-links" to="/flashcards">
-      Flashcards
-    </NavLink>
   </header>
 );
-
 export default Header;
